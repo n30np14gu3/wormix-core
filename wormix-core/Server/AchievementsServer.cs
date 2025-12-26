2f02e4236580b708aa6c57ad377e9919f0b09d3b
n30np14gu3 <darknetsoft@yandex.ru>
2025-12-26 18:26:52 +0300

    fix config names & add achievements server

---
+++ wormix-core/Server/AchievementsServer.cs
@@ -0,0 +1,12 @@
+﻿using wormix_core.Session;
+
+namespace wormix_core.Server;
+
+public class AchievementsServer(string address, int port) : TcpServer(address, port)
+{
+    protected override TcpSession CreateSession()
+    {
+        return new AchievementsSession(this);
+    }
+    
+}
---
+++ wormix-core/Session/AchievementsSession.cs
@@ -0,0 +1,8 @@
+﻿using wormix_core.Server;
+
+namespace wormix_core.Session;
+
+public class AchievementsSession(TcpServer server) : TcpSession(server)
+{
+    
+}
